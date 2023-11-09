eb8d69ca3ed45970b7b59290f60609f2b2965785
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-11-09 07:51:35 +0200

    Initialize 02-2-функції

---
+++ src/js/02-calculate-total-price.js
@@ -0,0 +1,12 @@
+/*
+ * Напиши функцию calculateTotalPrice(items)
+ * которая принимает массив цен (чисел) и возвращает их сумму
+ */
+
+const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+
+let total1 = calculateTotalPrice(cart);
+
+let total2 = calculateTotalPrice(cart);
+
+let total3 = calculateTotalPrice(cart);
