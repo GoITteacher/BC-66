37ef3d0aa405aa3a2eaa5f2d8709e7cfe80537e7
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-11-09 07:51:34 +0200

    Initialize 02-1-масиви

---
+++ src/js/04-find-login.js
@@ -0,0 +1,14 @@
+/*
+ * Напиши скрипт поиска логина
+ * - Если логина нет, вывести сообщение 'Пользователь [логин] не найден.'
+ * - Если нашли логин, вывести сообщение 'Пользователь [логин] найден.'
+ *
+ * - Сначала через for
+ * - Потом через for...of
+ * - Логика break
+ * - Метод includes() с тернарным оператором
+ */
+
+const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
+const firstLoginToFind = 'm4ngoDoge';
+const secondLoginToFind = 'k1widab3st';
---
+++ src/js/07-change-register.js
@@ -0,0 +1,8 @@
+/*
+ * Напиши скрипт который заменяет регистр каждого символа в строке на противоположный.
+ * Например, если строка «JavaScript», то на выходе должена быть строка «jAVAsCRIPT».
+ */
+
+const string = 'qWeRTzxCv';
+const letters = string.split('');
+let invertedString = '';
