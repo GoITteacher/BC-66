86e44ebd5f36ce72bc5690289eba766e5b0cec41
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-11-09 07:51:40 +0200

    Initialize 07-2-lazyload

---
+++ src/scripts/01-throttle-debounce.js
@@ -0,0 +1,6 @@
+const countMouseEventElement = document.querySelector('.js-count-mouse');
+const inputElement = document.querySelector('.js-input');
+const outputElem = document.querySelector('.js-output');
+
+let countMouseEvent = 0;
+let countInputEvent = 0;
---
+++ src/scripts/02-filter.js
@@ -0,0 +1,15 @@
+let list = [
+  { label: 'Html' },
+  { label: 'Css' },
+  { label: 'JavaScript' },
+  { label: 'Redux' },
+  { label: 'React' },
+  { label: 'Vue' },
+  { label: 'C++' },
+  { label: 'C#' },
+  { label: 'NodeJs' },
+  { label: 'Vanil' },
+];
+
+let filterElem = document.querySelector('.js-filter');
+let listElem = document.querySelector('.js-list');
