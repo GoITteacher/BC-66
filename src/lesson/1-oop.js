ecd91e59016fe42f39b3d350c8edc7abbfd1b9bf
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-11-22 12:34:39 +0200

    update

--- src/lesson/1-oop.js
+++ src/lesson/1-oop.js
@@ -6,11 +6,45 @@
 
 */
 
-////////////////////////////////////////////////
+// ========================
+
+// const father = {
+//   name: 'Vasya',
+// };
+
+// const child = Object.create(father);
+
+// const test = {
+//   __proto__: father,
+// };
+
+// child.x = 10;
+// child.y = 20;
+
+// console.log(child.name);
 
 /* 
 Сутності ООП
   - Клас
   - Екземпляр
   - Інтерфейс
-// */
+*/
+/* 
+function createCar() {
+  const car = {
+    color: '',
+    price: 0,
+    speed: 100,
+  };
+  return car;
+}
+
+const car1 = createCar();
+const car2 = createCar();
+const car3 = createCar();
+const car4 = createCar();
+
+console.log(car1);
+console.log(car2);
+console.log(car3);
+console.log(car4); */
--- src/lesson/3-class.js
+++ src/lesson/3-class.js
@@ -14,6 +14,90 @@
 
 */
 
+// class User {
+//   #age;
+
+//   constructor({ name, age, phoneNumber }) {
+//     this.name = name || 'Default';
+//     this.#age = age || 1;
+//     this.phoneNumber = phoneNumber || 'Default';
+//   }
+
+// showName() {
+//   console.log('Name', this.name);
+// }
+
+// get age() {
+//   return `Age: ${this.#age}`;
+// }
+
+// set bye(newValue) {
+
+// }
+// }
+
+// const user = new User({ name: 'Max', age: 10 });
+
+// console.log(user.age);
+
+// let x = 10;
+// console.log(x);
+// =====================
+
+// const user1 = {
+//   name: 'test1',
+//   showName() {
+//     console.log('Name', mthis.name);
+//   },
+// };
+
+// const user2 = {
+//   name: 'test2',
+//   showName() {
+//     console.log('Name', this.name);
+//   },
+// };
+
+// const user3 = {
+//   name: 'test3',
+//   showName() {
+//     console.log('Name', this.name);
+//   },
+// };
+
+// console.log(user1);
+// console.log(user2);
+// console.log(user3);
+
+// ==========================
+
+/* class User {
+  static count = 0;
+
+  constructor() {
+    this.name = '' + User.PI;
+    this.age = 0;
+    User.count += 1;
+  }
+
+  // static get PI() {
+  //   return User.#PI;
+  // }
+
+  static getTotal() {}
+} */
+
+// const user1 = new User();
+// const user2 = new User();
+// const user3 = new User();
+// const user4 = new User();
+// const user5 = new User();
+
+// console.log(user1);
+// console.log(user2);
+
+// console.log(User.count);
+
 /* 
 Синтаксис літерала об'єкта дозволяє створити один об'єкт. Проте, часто потрібно створити багато однотипних об'єктів з однаковим набором властивостей, але різними значеннями і методами для взаємодії з ними. Все це потрібно робити динамічно, під час виконання програми. З цією метою використовують класи - спеціальний синтаксис оголошення функції для створення об'єктів.
 */
@@ -38,3 +122,88 @@
 // };
 
 // =======================================
+
+class Animal {
+  #test = 10;
+  static count = 0;
+
+  showInfo() {
+    console.log(this.name);
+    console.log(this.age);
+  }
+
+  sleep() {
+    console.log('Zzzzz');
+  }
+
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    Animal.count += 1;
+  }
+
+  getTest1() {
+    console.log(this.#test);
+  }
+}
+
+class Cat extends Animal {
+  #test = 20;
+
+  constructor(name, age, paws) {
+    super(name, age);
+    this.name = 'Hello';
+    this.paws = paws;
+  }
+
+  sayMyaw() {
+    console.log('Myaw myaw');
+  }
+
+  sleep() {
+    console.log('Cat zzzzzz');
+  }
+
+  getTest2() {
+    console.log(this.#test);
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age) {
+    super(name, age);
+    this.name = 'Hello';
+    this.paws = 4;
+  }
+
+  sayMyaw() {
+    console.log('Myaw myaw');
+  }
+
+  sleep() {
+    console.log('Cat zzzzzz');
+  }
+}
+
+class Bird extends Animal {
+  constructor(name, age, paws) {
+    this.name = 'Hello';
+    this.wings = 2;
+  }
+
+  sayMyaw() {
+    console.log('Myaw myaw');
+  }
+
+  sleep() {
+    console.log('Cat zzzzzz');
+  }
+}
+
+const cat1 = new Cat('Marsil', 10, 4);
+const cat2 = new Cat('Marsil', 10, 5);
+const dog1 = new Dog('Marsil', 10, 4);
+const dog2 = new Dog('Marsil', 10, 5);
+
+console.log(Dog.count);
+console.log(Animal.count);
