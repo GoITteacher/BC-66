c890b24cbfc50047de84f5d4498e40b06a78c5d7
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-12-08 08:51:54 +0200

    update

--- src/const.js
+++
@@ -1,4 +0,0 @@
-import 'dotenv/config';
-require('dotenv').config();
-
-export const PASSWORD = process.env.PASSWORD;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-import './css/common.css';
-import { PASSWORD } from './const';
-console.log(PASSWORD);
+const myPromise = delay =>
+  new Promise((res, rej) => {
+    setTimeout(res, delay);
+  });
+
+setTimeout(() => console.log('1'), 1000);
+myPromise(1000).then(res => console.log('2'));
+setTimeout(() => console.log('3'), 100);
+myPromise(2000).then(res => console.log('4'));
+setTimeout(() => console.log('5'), 2000);
+myPromise(1000).then(res => console.log('6'));
+setTimeout(() => console.log('7'), 1000);
+myPromise(5000).then(res => console.log('8'));
